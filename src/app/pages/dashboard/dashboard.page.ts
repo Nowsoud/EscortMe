539323fd05e4c222f4f79b29ba1c755c9496a914
Konstantin Latest <nowsoud@gmail.com>
2019-06-01 12:06:44 +0200

    qr code update

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -4,7 +4,7 @@ import { AuthenticationService } from '../../services/authentication/authenticat
 import { ToastService } from 'src/app/services/toast/toast.service';
 import { UserService } from 'src/app/services/user/user.service';
 import { FriendsService } from 'src/app/services/friends/friends.service';
-import { Map, latLng, tileLayer, Layer, marker, icon } from 'leaflet';
+import { videoOverlay, Map, latLng, tileLayer, Layer, marker, icon } from 'leaflet';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 import { DeviceMotion, DeviceMotionAccelerationData } from '@ionic-native/device-motion/ngx';
 import { Storage } from '@ionic/storage';
@@ -25,7 +25,7 @@ export class DashboardPage implements OnInit {
   constructor(
     private userService: UserService,
     private friendsService: FriendsService,
-    private toast:ToastService,
+    private toast: ToastService,
     private geolocation: Geolocation,
     private deviceMotion: DeviceMotion,
     private storage: Storage
@@ -33,68 +33,69 @@ export class DashboardPage implements OnInit {
 
   ngOnInit() {
     this.userService.getUserInfo()
-      .then(res=>
-      {
+      .then(res => {
         this.geolocation.watchPosition().subscribe((data) => {
-          if(data.coords){
-              this.userService.updateUserGeo([data.coords.latitude, data.coords.longitude])
-                .then(res=>this.PointUserMarker())
-                .catch(err=>this.toast.present(err))
+          if (data.coords) {
+            this.userService.updateUserGeo([data.coords.latitude, data.coords.longitude])
+              .then(res => this.PointUserMarker())
+              .catch(err => this.toast.present(err))
           }
           else console.log(data);
         });
 
         this.deviceMotion.watchAcceleration({ frequency: 250 }).subscribe((data) => {
           this.stateTest = data.x + " " + data.y + " " + data.z;
-          this.motionCache.push([data.x,data.y,data.z]);
+          this.motionCache.push([data.x, data.y, data.z]);
 
-          if(this.motionCache.length >= 8){
-            
+          if (this.motionCache.length >= 8) {
+
             //todo: compute state
             var state = "standing"
 
             this.userService.updateUserState(state);
-            delete(this.motionCache)
+            delete (this.motionCache)
             this.motionCache = new Array<number[]>();
           }
 
         });
       })
-      .catch(err=>this.toast.present(err));
+      .catch(err => this.toast.present(err));
   }
-  
+
   Loadmap() {
-      return new Promise((resolve, reject)=>{
-        this.userService.getUserInfo().then(user => {
-          this.userInfo = user
-          this.map = new Map('map').setView(this.userInfo.geo, 12);
-          tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', 
+    return new Promise((resolve, reject) => {
+      this.userService.getUserInfo().then(user => {
+        this.userInfo = user
+        this.map = new Map('map').setView(this.userInfo.geo, 12);
+        tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
           { maxZoom: 18, }).addTo(this.map);
-          resolve()
-        })
-      }) 
+
+        resolve()
+      })
+    })
   }
   PointUserMarker() {
+    
+    var m_icon = icon({
+      iconUrl: "https://firebasestorage.googleapis.com/v0/b/escortme-2c3d1.appspot.com/o/map_icon.png?alt=media&token=bbd4daaa-006e-4b2c-a01a-2fb631f0bced",
+      iconSize: [35, 35],
+      popupAnchor: [3, -20]
+    });
+
     if (this.userInfo) {
-      if (!this.currentMarker) {
-        var m_icon = icon({
-          iconUrl: "https://firebasestorage.googleapis.com/v0/b/escortme-2c3d1.appspot.com/o/ninja-portable.png?alt=media&token=6539eaca-592d-498a-a4ca-a2d8596d2db3",
-          iconSize: [40, 40],
-          popupAnchor: [3, -20]
-        });
+      if (!this.currentMarker) 
         this.currentMarker = marker(this.userInfo.geo, { icon: m_icon })
-        this.currentMarker.addTo(this.map)
-          .bindPopup(`<b>${this.userInfo.name}</b>  <p>${this.userInfo.state}</p>`);
-      }else{
+          .bindPopup(`<b>${this.userInfo.name}</b><p>${this.userInfo.state}</p>`);
+       else 
         this.currentMarker.setLatLng(this.userInfo.geo)
-      }
+        this.currentMarker.addTo(this.map)
     }
   }
   ionViewWillLeave() {
     this.map.remove();
   }
-  
-  ionViewWillEnter(){
-    this.Loadmap().then(()=>this.PointUserMarker())
+
+  ionViewWillEnter() {
+    this.Loadmap().then(() => this.PointUserMarker())
   }
 }
